03f312b323bc589c63d6d1cc0cb279ad437252ad
mroszkow <svensonie@gmail.com>
2023-02-25 22:03:43 +0100

    Wykoanie zmian zgodnie z Develop (PR #1)

--- fib_seq.py
+++ fib_seq.py
@@ -2,13 +2,9 @@ def feb_seq_item(n: int) -> int:
     fibonacci_numbers = []
     one_step_back = 1
     two_step_back = 0
-    for i in range(n+1):
-
-        
-        if i == 0:
-            item = 0
-        elif i == 1:
-            item = 1
+    for i in range(n + 1):
+        if i in (0, 1):
+            item = i
         elif i > 1:
             item = one_step_back + two_step_back
             two_step_back, one_step_back = one_step_back, item
