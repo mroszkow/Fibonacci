cc8e74efe0421219866efa54bc89a660df8a90e4
mroszkow <svensonie@gmail.com>
2023-02-27 21:10:38 +0100

    Unittest of fib_seq was added

---
+++ test_fib_seq_ut.py
@@ -0,0 +1,41 @@
+import unittest
+from fib_seq import feb_seq_item
+
+
+class TestFib_seq_item(unittest.TestCase):
+    def test_fib_seq_item(self):
+        self.assertEqual(feb_seq_item(0), [0])
+        self.assertEqual(feb_seq_item(1), [0, 1])
+        self.assertEqual(
+            feb_seq_item(19),
+            [
+                0,
+                1,
+                1,
+                2,
+                3,
+                5,
+                8,
+                13,
+                21,
+                34,
+                55,
+                89,
+                144,
+                233,
+                377,
+                610,
+                987,
+                1597,
+                2584,
+                4181,
+            ],
+        )
+        self.assertEqual(feb_seq_item(-5), [])
+
+        with self.assertRaises(TypeError):
+            feb_seq_item(1.8)
+
+
+if __name__ == "__main__":
+    unittest.main()
